2b2b7514a3cb3469c3c265521b8ca06783cb1e76
blazerpaul15 <blazerpaul15@gmail.com>
2022-12-20 15:06:27 +0000

    vili: Arrow

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,3 @@
-
 #
 # Copyright (C) 2021 The LineageOS Project
 #
@@ -6,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_vili.mk
+    $(LOCAL_DIR)/arrow_vili.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_vili-user \
-    lineage_vili-userdebug \
-    lineage_vili-eng
+    arrow_vili-user \
+    arrow_vili-userdebug \
+    arrow_vili-eng
--- device.mk
+++ device.mk
@@ -24,7 +24,6 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
